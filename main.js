a895c8bff3b7bf3906505b8177946cb95c81220f
Sarah Lopez <slopez323@gmail.com>
2022-03-23 13:52:24 -0400

    completed

--- main.js
+++ main.js
@@ -1,73 +1,56 @@
-/*******************
- * OUR HELPER CODE *
-*******************/
+const color = document.querySelectorAll('.palette-color');
+const brushColor = document.querySelector('.current-brush');
+let canvasSquares;
+let currentColor;
+let mouseDown = false;
+createGrid(10);
 
-/*
- * Here we add the squares to the canvas dynamically.
- * You can mostly leave this section alone!
- * But if you want to change how wide the canvas is,
- * there are just two steps:
- * 
- * 1. Change the `gridWidth` value below.
- * 2. Change the `grid-template-rows` and
- * `grid-template-columns` to match.
- *
- * To make the second one happen, the number to change
- * is the first argument to `repeat`, currently set at 10.
- */
-const gridWidth = 10;
-let count = 0;
-while (count <= gridWidth * gridWidth) {
+function createGrid(width) {
   const canvas = document.querySelector('.canvas');
-  const div = document.createElement('div');
-  div.className = 'square color-5';
-  canvas.appendChild(div);
-  count++;
-}
+  canvas.innerHTML = '';
+  const gridWidth = width;
+  let count = 0;
+  while (count <= gridWidth * gridWidth) {
+    const div = document.createElement('div');
+    div.className = 'square color-5';
+    canvas.appendChild(div);
+    count++;
+  }
+  canvasSquares = document.querySelectorAll('.square');
 
-// You probably should NOT do these in the order below.
-// That is, you probably should NOT do all the queries,
-// THEN all the functions,
-// THEN all the wiring.
+  canvas.style.gridTemplateRows = `repeat(${width},1fr)`
+  canvas.style.gridTemplateColumns = `repeat(${width},1fr)`
+  addListeners();
+};
 
-// Instead, it'll be easier if you go one action at a time!
-// So, add a query for the palette colors.
-// THEN add an event listener function for what happens when one is clicked.
-// THEN wire those two together, so that when the palette elements are clicked,
-// the function runs.
-//
-// And proceed from there to getting the squares working.
-//
+function addListeners(){
+color.forEach(x => x.addEventListener('click', function () {
+  currentColor = x.classList[1];
+  brushColor.classList.replace(brushColor.classList[1], currentColor);
+}));
 
-// ALSO.
-// You do not have to follow the sections below. If you're doing your functions inline, it doesn't make a lot of sense to separate the event listener functions from their wiring!
+canvasSquares.forEach(x => x.addEventListener('click', function () {
+  x.classList.replace(x.classList[1], currentColor);
+}));
 
-/***********
- * QUERIES *
-***********/
+canvasSquares.forEach(x => x.addEventListener('mouseenter', function () {
+  if (mouseDown === true) x.classList.replace(x.classList[1], currentColor);
+}));
+};
 
-// Add queries for all your squares, palette colors, and brush here.
-// (Note the singular or plural used in that sentence!)
+window.addEventListener('mousedown', function () {
+  mouseDown = true;
+});
 
+window.addEventListener('mouseup', function () {
+  mouseDown = false;
+});
 
+const slider = document.getElementById("myRange");
+const output = document.querySelectorAll('.slidevalue');
+output.forEach(x => x.innerText = slider.value);
 
-/****************************
- * EVENT LISTENER FUNCTIONS *
-****************************/
-
-// Now add some functions to handle clicking one particular square
-// and clicking one particular palette color. You can leave them
-// empty at first, though a console.log just to know they're being
-// run as event listeners (after the next step is set up) isn't a
-// bad idea for testing purposes.
-
-
-
-/**************************
- * WIRING IT ALL TOGETHER *
-**************************/
-
-// Now: wiring up our event listeners to our html node elements.
-// You'll need to add the appropriate event listener for each
-// square and for each palette color from the functions you
-// wrote above.
+slider.oninput = function () {
+  output.forEach(x => x.innerText = slider.value);
+  createGrid(slider.value)
+};
